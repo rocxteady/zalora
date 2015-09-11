d2820fc0caad6c6df5c2236eb3292b8f01ddfb8b
Ulaş Sancak <ulas@Metuss-MacBook-Pro.local>
2015-09-11 16:25:34 +0300

    Initial Commit

---
+++ Zalora/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zalora
+//
+//  Created by Ulaş Sancak on 11/09/15.
+//  Copyright (c) 2015 Ulaş Sancak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Zalora/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zalora
+//
+//  Created by Ulaş Sancak on 11/09/15.
+//  Copyright (c) 2015 Ulaş Sancak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
